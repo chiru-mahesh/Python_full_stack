f10f9bbce2921c0d47d342dba3b04fe2ade3e70a
chiru-mahesh <mahesh520004@gmail.com>
2026-02-18 19:19:02 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,126 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#variables
+
+print(4+7)
+11
+
+a=2
+print(a)
+2
+
+b=4
+print(b)
+4
+
+c=100
+print(c)
+100
+
+x=20
+print(20)
+20
+
+z=50
+print(z)
+50
+
+a012345678910=200
+print(a012345678910)
+200
+
+a2=9
+print(a2)
+9
+
+b9=6
+print(b9)
+6
+
+
+city="vij"
+print(city)
+vij
+
+name="mahesh"
+print(name)
+mahesh
+
+colour="blue"
+print(colour)
+blue
+
+country="india"
+print(country)
+india
+
+first name="mahesh"
+SyntaxError: invalid syntax
+firstname="mahesh"
+
+
+first_name="mahesh"
+print(first_name)
+mahesh
+
+-a=20
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+a=20
+print(a)
+20
+
+
+
+
+#singleline2variables
+
+a,b=3,4
+print(a+b)
+7
+
+>>> a=5
+>>> b=8
+>>> print(a+b)
+13
+>>> 
+>>> a=6,b=8
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> a=6; b=8
+>>> print(a+b)
+14
+>>> 
+>>> 
+>>> fname,lname= "mahesh","g"
+>>> print(fname+lname)
+maheshg
+>>> 
+>>> print(fname+" "+lname)
+mahesh g
+>>> print(fname, lname)
+mahesh g
+>>> 
+>>> 
+>>> fname="mahesh"; lname="g"
+>>> print(fname+lname)
+maheshg
+>>> 
+>>> 
+>>> #deletekeyword
+>>> 
+>>> a=6
+>>> print(a)
+6
+>>> 
+>>> name="mahesh"
+>>> print(name)
+mahesh
+>>> 
+>>> age=27
+>>> print(age)
+27
+>>> 
+>>> AGE=26
+>>> print(AGE)
+26
+>>> 
+>>> 
