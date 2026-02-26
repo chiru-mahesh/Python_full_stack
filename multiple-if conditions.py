42d56caf55846d26e4c7efd021909bdd8b038dc3
chiru-mahesh <mahesh520004@gmail.com>
2026-02-26 19:46:18 +0530

    Add files via upload

---
+++ multiple-if conditions.py
@@ -0,0 +1,185 @@
+#multiple-if conditions by using comparision opertors
+
+'''a=5
+b=10
+if a<b:
+    print("less")
+if b>a:
+    print("greater")
+if a!=b:
+    print("not equal")'''
+
+
+
+
+
+'''a=5
+b=10
+if a<b:
+    print("less")
+elif b>a:
+    print("greater")
+elif a!=b:
+    print("not equal")'''
+
+
+
+
+'''a=5
+b=10
+if a==b:
+    print("less")
+if b>a:
+    print("greater")
+elif a!=b:
+    print("not equal")'''
+
+
+
+
+'''a=5
+b=10
+if a<b:
+    print("less")
+if b>a:
+    print("greater")
+if a==b:
+    print("not equal")
+elif a!=b:
+    print("true")'''
+
+
+
+
+'''a=5
+b=10
+if a<b and b>a:
+    print("less")
+if b>a and b==a:
+    print("greater")
+if a>b and a<=b:
+    print("not equal")
+elif a!=b:
+    print("true")'''
+
+
+
+'''a=5
+b=10
+if a<b and b>a:
+    print("less")
+if b>a or b==a:
+    print("greater")
+elif  not a!=b:
+    print("true")'''
+
+
+
+
+
+'''a=5
+b=3.0
+if type(a) is int:
+    print("less")
+if type(b) is int:
+    print("greater")
+if  type(a) is not float:
+    print("true")'''
+
+
+
+
+
+'''a=[1,2,3,4,5,6]
+b=int(input())
+if 10 not in a:
+    print("true")
+
+if 6 in a:
+    print("false")'''
+
+
+
+
+#nested-if conditions
+
+
+'''a=10
+b=20
+if a<b:
+    print("less")
+    if b>a:
+        print("greater")'''
+
+
+
+
+'''a=10
+b=20
+if a==b:
+    print("less")
+    if b>a:
+        print("greater")'''
+
+
+
+'''a=10
+b=20
+if a<=b:
+    print("less")
+    if b==a:
+       print("greater")
+    else:
+        print("true")'''
+
+
+
+'''a=10
+b=20
+if a==b:
+    print("less")
+    if b!=a:
+       print("greater")
+else:
+    print("true")'''
+
+
+
+'''a=10
+b=20
+if a<=b:
+    print("less")
+    if b==a:
+       print("greater")
+    else:
+       print("true")
+else:
+    print("false")'''
+
+
+
+'''a=10
+b=20
+if a<=b:
+    print("less")
+    if b==a:
+       print("greater")
+    elif a==b:
+       print("not equal")
+    else:
+       print("true")'''
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
+
