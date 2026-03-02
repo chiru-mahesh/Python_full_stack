6d3c030412ab6cd50820663dc0869bb195dbeba6
chiru-mahesh <mahesh520004@gmail.com>
2026-03-02 17:52:13 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,261 @@
+#loops()
+#for(),while(),break,range(),continue,pass
+
+#for using in lits()
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)'''
+
+
+
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(a)'''
+
+
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,end=",")'''
+
+
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+
+
+
+
+
+#for using in tuple()
+
+'''a=(10,20,30,40,50)
+for i in a:
+    print(i)'''
+    
+
+
+'''a=(10,20,30,40,50)
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+
+
+
+'''a=(10,20,30,40,50)
+for i in a:
+    print(i)
+    print(type(a))'''
+
+
+
+
+
+
+#for using in sets()
+
+'''a={4,5,6,7,8,9}
+for i in a:
+    print(i)'''
+
+
+
+
+
+'''a={4,5,6,7,8,9}
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+
+
+
+#for using in dict()
+
+'''a={"name":"mahesh","year":2026,"month":3}
+for i in a:
+    print(i)'''
+    
+
+
+
+'''a={"name":"mahesh","year":2026,"month":3}
+for i in a:
+    print(i)
+for i in a.keys():
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+
+
+
+'''a={"name":"mahesh","year":2026,"month":3}
+for i in a:
+    print(i)
+for i in a.values():
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+
+
+
+
+'''a={"name":"mahesh","year":2026,"month":3}
+for i in a:
+    print(i)
+for i in a.items():
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+
+
+
+#for using in string()
+
+'''a="codegnan"
+for i in a:
+    print(i)'''
+
+
+
+
+
+'''a="codegnan"
+for i in a:
+    print(i,end=" ")
+    print(type(a))
+    print(type(i))'''
+
+
+
+
+
+
+
+#a=["code","python","course"]
+#CODE,PYTHON,COURSE
+
+
+
+
+'''a=["code","python","course"]
+for i in a:
+    print(i.upper(),end=" ")'''
+
+
+
+
+'''a=["code","python","course"]
+b=str(a)
+print(b.upper())'''
+    
+
+
+
+
+
+
+#while loop()
+
+
+'''a=10
+while a>1:
+    print(a)'''
+
+
+
+'''a=20
+while a>1:
+    print(a)
+    a=a-1'''
+
+
+
+'''a=20
+while a>=1:
+    print(a)
+    a=a-1'''
+
+
+
+
+'''a=20
+while a>1:
+    a=a-1
+    print(a)'''
+    
+
+
+
+'''a=20
+while a>1:
+    a=a-1
+print(a)'''
+
+
+
+'''a=15
+while a>=2:
+    print(a)
+    a-=1'''
+
+
+
+'''a=15
+while a>=2:
+    print(a)
+    a+=1'''
+
+
+
+'''a=3
+while a>=18:
+    print(a)
+    a*=1'''
+
+
+
+
+#voting using while loop()
+
+
+'''while True:
+    age=int(input("enter the age"))
+    if age>=18:
+        print("18+ eligible for vote")
+    else:
+        print("below not eligible")'''
+
+
+
+#even and odd using while loop()
+
+'''while True:
+    a=int(input("enter value a"))
+    if a%2==0:
+        print("a is even")
+    else:
+        print("a is odd")'''  
+
+
+
+#leapyear using while loop()
+
+'''while True:
+    year=int(input("enter a year"))
+    if year%4==0:
+        print("it is leap year")
+    else:
+        print("it is not leap year")'''    
+
